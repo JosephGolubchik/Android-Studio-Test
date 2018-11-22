2f9ec8f721fb6111528888011b7f2ad5e979d128
Joseph Golubchik <yosi_goloo@walla.co.il>
2018-11-22 19:59:41 +0200

    First commit

---
+++ app/src/main/java/com/example/yosi/helloworld/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.yosi.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = findViewById(R.id.secondNumEditText);
+                TextView resultTextView = findViewById(R.id.resultTextView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+
+                resultTextView.setText("" + result);
+            }
+        });
+    }
+}
